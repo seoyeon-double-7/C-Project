5ea5f209a4179b06c366d9540109c13cbe2db414
SeoyeonBae <83990943+cindy0817-web@users.noreply.github.com>
2022-01-04 13:45:22 +0900

    Add files via upload

---
+++ After-class/test01.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int n;
+
+int main() {
+
+	while (1) {
+	int sum = 0;
+		printf("숫자 입력 : ");
+		scanf_s("%d", &n);
+			
+		if (n == 0) 
+			break;
+
+		else if(n % 2 == 1) {
+			for (int i = 1; i < n; i++) 
+				if (i % 2 == 1)
+					sum += i;
+			
+			printf("%d\n", sum);
+		}
+		else
+			printf("%d\n", n * n * n);
+	}
+}
