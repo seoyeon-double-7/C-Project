66e0da08d66bc4a5d17e8024f7a78129c6c0b40a
Seoyeon Bae <83990943+seoyeon-double-7@users.noreply.github.com>
2022-01-11 16:04:14 +0900

    구조체 배열 함수의 매개변수로 보내서 입출력 3번 받기

--- After-class/structure.c
+++ After-class/structure.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+#include <stdio.h>
 #define _CRT_SECURE_NO_WARNINGS
 
 typedef struct _Person {
@@ -7,25 +7,36 @@ typedef struct _Person {
 	int age;
 } Person;
 
-int main() {
-	Person arr[3] = {"", 0, " "};
-	char n[2];
+void inPerson(Person* p);
+void outPerson(Person* p);
 
+int main() {
+	Person arr[3];
+	inPerson(arr);
+	outPerson(arr);
+	return 0;
+}
+
+void inPerson(Person *p) {
+	char n[2];
+	printf("=========== 입력부분 ===========\n");
 	for (int i = 0; i < 3; i++) {
 		printf("이름 입력 : ");
-		gets(arr[i].name);
+		gets((p+i)->name);
 
 		printf("전화번호 입력 : ");
-		gets(arr[i].phone_num);
+		gets((p+i)->phone_num);
 
 		printf("나이 입력 : ");
-		scanf_s("%d", &arr[i].age);
-
-		printf("이름: %s\n", arr[i].name);
-		printf("전화번호: %s\n", arr[i].phone_num);
-		printf("나이: %d\n", arr[i].age);
-		gets_s(n, 2);
-		}
-	
-	return 0;
+		scanf_s("%d", &(p+i)->age);
+		gets(n);
+	}	
+}
+void outPerson(Person *p) {
+	printf("=========== 출력부분 ===========\n");
+	for (int i = 0; i < 3; i++) {
+		printf("이름: %s\n", (p + i)->name);
+		printf("전화번호: %s\n", (p + i)->phone_num);
+		printf("나이: %d\n\n", (p + i)->age);
+	}
 }
