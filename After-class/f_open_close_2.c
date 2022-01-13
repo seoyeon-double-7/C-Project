f4a6c75956852df1ed0743712672bc83789b2bdd
Seoyeon Bae <83990943+seoyeon-double-7@users.noreply.github.com>
2022-01-13 15:27:22 +0900

    파일 입출력

---
+++ After-class/f_open_close_2.c
@@ -0,0 +1,15 @@
+﻿#include<stdio.h>
+
+int main() {
+	char a[100] = "good morning";
+	FILE *fp;
+	fp = fopen("a.txt","w");
+
+	if (fp == NULL) puts("no file");
+	else puts("success! check your file");
+
+	fputs(a, fp);
+
+	fclose(fp);
+	return 0;
+}
---
+++ After-class/f_open_close_3.c
@@ -0,0 +1,20 @@
+﻿#include<stdio.h>
+
+int main() {
+	char name[15];
+//	char tel[15];
+	
+
+	FILE *fp;
+	fp = fopen("a.txt","a+");
+	if (fp == NULL) puts("open file");
+
+	fgets(name, 15, stdin);
+//	fgets(tel, 15, stdin);
+
+	fputs(name, fp);
+//	fputs(tel, fp);
+
+	fclose(fp);
+	return 0;
+}
